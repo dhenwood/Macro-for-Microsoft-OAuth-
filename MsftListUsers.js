d17e505581417e0c114d1823bbfd0d298c849c55
dhenwood <dhenwood@users.noreply.github.com>
2026-04-22 14:59:17 +1000

    Create MsftListUsers.js

---
+++ MsftListUsers.js
@@ -0,0 +1,33 @@
+import xapi from 'xapi';
+import {getValidAccessToken} from './MsftManageTokens';
+
+async function lookupPeople(){
+  let url = "https://graph.microsoft.com/v1.0/users"
+  let token = await getValidAccessToken()
+  let header = `Authorization: Bearer ${token}`
+
+  try {
+    const response = await xapi.Command.HttpClient.Get({
+        AllowInsecureHTTPS: "True",
+        Header: header,
+        ResultBody: "PlainText",
+        Url: url
+    });
+
+    let responseBody = response.Body
+    let data = JSON.parse(responseBody)
+    if (!data.value) {
+      throw new Error('Invalid data response');
+    }else{
+      let items = data.value
+      items.forEach((item) => {
+        let displayName = item.displayName
+        console.log("displayName: " + displayName)
+      })
+    }
+  } catch (err) {
+    console.error('Refresh failed:', err);
+  }
+}
+
+lookupPeople()
